35eccf7a9769c0873d3d9a771c1e88301e10f68f
porstuart <stuart_por@soc.uum.edu.my>
2019-11-08 12:32:20 +0800

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:lab_2/loginScreen.dart';
+import 'loginScreen.dart';
 
 void main() => runApp(MaterialApp(
       theme:
